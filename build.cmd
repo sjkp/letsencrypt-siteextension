906a2491705ae85c626cc3a3f232c922c92e43b3
Simon J.K. Pedersen <mail@sjkp.dk>
2018-11-11 11:54:33 +0100

    Fix build script for devops pipeline.

--- build.cmd
+++ build.cmd
@@ -2,9 +2,13 @@ nuget restore LetsEncrypt-SiteExtension.sln
 
 RMDIR /S /Q artifacts
 md artifacts
-"C:\Program Files (x86)\MSBuild\14.0\bin\MSBuild.exe" LetsEncrypt-SiteExtension\LetsEncrypt.SiteExtension.csproj /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="..\artifacts";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;SolutionDir="."
-"C:\Program Files (x86)\MSBuild\14.0\bin\MSBuild.exe" LetsEncrypt.SiteExtension.Core\LetsEncrypt.Azure.Core.csproj /p:Configuration=Release;SolutionDir="."
-"C:\Program Files (x86)\MSBuild\14.0\bin\MSBuild.exe" LetsEncrypt.SiteExtension.WebJob\LetsEncrypt.SiteExtension.WebJob.csproj /p:Configuration=Release;SolutionDir="."
+
+WHERE msbuild 
+IF %ERRORLEVEL% NEQ 0 SET PATH=%PATH%;"C:\Program Files (x86)\MSBuild\14.0\bin\MSBuild.exe"
+
+MSBuild LetsEncrypt-SiteExtension\LetsEncrypt.SiteExtension.csproj /t:pipelinePreDeployCopyAllFilesToOneFolder /p:_PackageTempDir="..\artifacts";AutoParameterizationWebConfigConnectionStrings=false;Configuration=Release;SolutionDir="."
+MSBuild LetsEncrypt.SiteExtension.Core\LetsEncrypt.Azure.Core.csproj /p:Configuration=Release;SolutionDir="."
+MSBuild LetsEncrypt.SiteExtension.WebJob\LetsEncrypt.SiteExtension.WebJob.csproj /p:Configuration=Release;SolutionDir="."
 xcopy LetsEncrypt.SiteExtension.Core\bin\Release\*.* artifacts\bin\ /sy
 
 md artifacts\app_data\jobs\continuous\letsencrypt.siteextension.job
